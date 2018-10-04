96542835b119fdbc659c7071524e99b3d4accaf3
Hemant <hsoni0303@gmail.com>
2018-10-04 13:35:17 +0530

    first assignment

---
+++ assignment1/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+<ul>
+  <Names v-for="name in names_arr" v-bind:item="name"></Names>
+</ul>
+</template>
+
+<script>
+import Names from './components/Names.vue';
+
+export default {
+  name: 'app',
+  components: {
+    Names
+  },
+  data() {
+    return {
+      names_arr: ['John', 'Jack', 'Jessy', 'Bunny', 'James'],
+    }
+  }
+}
+</script>
---
+++ assignment1/src/components/Names.vue
@@ -0,0 +1,12 @@
+<template>
+<li>
+  {{ item }}
+</li>
+</template>
+
+<script>
+  export default {
+    name: 'Names',
+    props: ['item'],
+  }
+</script>
