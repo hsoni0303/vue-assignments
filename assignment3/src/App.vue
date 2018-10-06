11ea927046210809363c9e906491c118e46855a1
Hemant <hsoni0303@gmail.com>
2018-10-06 08:43:19 +0530

    assignment3 submission

---
+++ assignment3/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <table>
+      <tr>
+        <th>
+          Names <button @click="sorted">Sort</button>
+        </th>
+      </tr>
+      <Names v-for="name in names_arr" v-bind:item="name" :key="name"></Names>
+    </table>
+  </div>
+</template>
+
+<script>
+import Names from './components/Names.vue';
+
+export default {
+  name: 'app',
+  components: {
+    Names,
+  },
+  data() {
+    return {
+      names_arr: ['John', 'Adam', 'Kyle', 'Carlos', 'Zed'],
+      toggle_button: false,
+    };
+  },
+  methods: {
+    sorted() {
+      if (!this.toggle_button) {
+        this.names_arr = this.names_arr.sort();
+        this.toggle_button = !this.toggle_button;
+      } else {
+        this.names_arr = this.names_arr.reverse();
+        this.toggle_button = !this.toggle_button;
+      }
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ assignment3/src/components/Names.vue
@@ -0,0 +1,26 @@
+<template>
+    <tr>
+      <td :class="['list-item']">
+        {{ item }}
+      </td>
+    </tr>
+</template>
+
+<script>
+export default {
+  name: 'Names',
+  props: ['item'],
+};
+</script>
+
+<style lang="css">
+.list-item {
+  padding: 20px;
+  background: #333;
+  color: white;
+  width: 100px;
+}
+.list-item:hover {
+  background-color: lightgreen;
+}
+</style>
